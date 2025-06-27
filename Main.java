1198341f33720416033180c444f5fbdbbedf12c4
mohammedashif-2004 <mohammadasif182004@gmail.com>
2025-06-27 15:19:15 +0530

    Added bubble sort logic in sorting-logic branch

--- Main.java
+++ Main.java
@@ -2,21 +2,37 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
+
+        // sorting the array through bubble sort
+    public static void sortArray(int[] number){
+
+        for(int i=0 ; i< number.length-1 ; i++){
+             boolean swapped = false;
+            for(int j=0 ; j< number.length-1 ; j++){
+                if(number[j] > number[j+1]){
+                int temp = number[j];
+                number[j] = number[j+1];
+                number[j+1] = temp;
+                swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
         public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         // taking input from the user and storing in the array
 
-        System.out.println("enter number of elements to add in array");
-        int n = sc.nextInt();
+        System.out.println("enter 5 numbers in array");
 
-        int[] number = new int[n];
-        System.out.println("enter "+n+" numbers");
-        for(int i=0 ; i<n ; i++){
+        int[] number = new int[5];
+        for(int i=0 ; i<5 ; i++){
             number[i] = sc.nextInt();
         }
         System.out.println("number stored in array are");
 
-        for(int i=0 ; i<n ; i++){
+        for(int i=0 ; i<5 ; i++){
             System.out.println(number[i]);
         }
 
