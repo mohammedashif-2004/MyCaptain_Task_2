cb682a7742579e5e82d76a791eff8feab3d277ed
mohammedashif-2004 <mohammadasif182004@gmail.com>
2025-06-27 16:37:39 +0530

    Updated sorting logic in sorting-logic branch

--- Main.java
+++ Main.java
@@ -2,24 +2,6 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-
-        // sorting the array through bubble sort
-    public static void sortArray(int[] number){
-
-        for(int i=0 ; i< number.length-1 ; i++){
-             boolean swapped = false;
-            for(int j=0 ; j< number.length-1 ; j++){
-                if(number[j] > number[j+1]){
-                int temp = number[j];
-                number[j] = number[j+1];
-                number[j+1] = temp;
-                swapped = true;
-                }
-            }
-            if (!swapped) break;
-        }
-    }
-
         public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         // taking input from the user and storing in the array
@@ -36,9 +18,18 @@ public class Main {
             System.out.println(number[i]);
         }
 
-        System.out.println("sorted array:");
-        for(int num : number){
-            System.out.println(num+" ");
+                // sorting the array through bubble sort
+
+        for(int i=0 ; i< number.length-1 ; i++){
+             boolean swapped = false;
+            for(int j=0 ; j< number.length-1 ; j++){
+                if(number[j] > number[j+1]){
+                int temp = number[j];
+                number[j] = number[j+1];
+                number[j+1] = temp;
+                swapped = true;
+                }
+            }
         }
         
 }
